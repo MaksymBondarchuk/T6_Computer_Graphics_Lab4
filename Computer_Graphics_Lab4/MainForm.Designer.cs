649218382252825148e190307c60c2fce4b25b2c
Maxim <maksim.bondarchuk@gmail.com>
2015-05-07 14:41:48 +0300

    Starting

---
+++ Computer_Graphics_Lab4/MainForm.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Computer_Graphics_Lab4
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbox = new System.Windows.Forms.PictureBox();
+            this.bBuild = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbox
+            // 
+            this.pbox.BackColor = System.Drawing.Color.Gainsboro;
+            this.pbox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbox.Location = new System.Drawing.Point(12, 12);
+            this.pbox.Name = "pbox";
+            this.pbox.Size = new System.Drawing.Size(1000, 300);
+            this.pbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbox.TabIndex = 2;
+            this.pbox.TabStop = false;
+            // 
+            // bBuild
+            // 
+            this.bBuild.Location = new System.Drawing.Point(920, 443);
+            this.bBuild.Name = "bBuild";
+            this.bBuild.Size = new System.Drawing.Size(75, 23);
+            this.bBuild.TabIndex = 3;
+            this.bBuild.Text = "Build";
+            this.bBuild.UseVisualStyleBackColor = true;
+            this.bBuild.Click += new System.EventHandler(this.bBuild_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1040, 498);
+            this.Controls.Add(this.bBuild);
+            this.Controls.Add(this.pbox);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbox;
+        private System.Windows.Forms.Button bBuild;
+    }
+}
+
---
+++ Computer_Graphics_Lab4/MainForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Computer_Graphics_Lab4
+{
+    public partial class MainForm : Form
+    {
+        const int SizeX = 10000;
+        const int SizeY = 3000;
+        Bitmap _bmp = new Bitmap(SizeX, SizeY);
+        Pen _pen = new Pen(Color.Black);
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        void Koch(int rankCurrent)
+        {
+            if (rankCurrent == 1)
+            {
+                var g = Graphics.FromImage(_bmp);
+                var start = new Point(100, SizeY - 100);
+                var end = new Point(SizeX - 100, SizeY - 100);
+                g.DrawLine(_pen, start, end);
+            }
+        }
+
+        private void bBuild_Click(object sender, EventArgs e)
+        {
+            Koch(1);
+
+            pbox.Image = _bmp;
+
+            _bmp.Save("1.bmp");
+        }
+    }
+}
